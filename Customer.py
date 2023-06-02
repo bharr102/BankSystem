8402a9f4c4cc385d9f1c211e70bd93d9f8bfe385
bharr102_GitHub <bharr102@mtroyal.ca>
2023-06-02 16:12:20 -0600

    creating the customer object.

---
+++ Customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    def __init__(self, name, age, email, occupation):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.occupation = occupation
+

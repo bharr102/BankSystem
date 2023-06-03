702cbed3f68269d9329a9e79b5d4e1eebd46a357
bharr102_GitHub <bharr102@mtroyal.ca>
2023-06-02 20:27:50 -0600

    Made a new folder to create classes that work to get the users input/output and display menu options

---
+++ src/UserIO/Menu.py
@@ -0,0 +1,8 @@
+class Menu:
+    def __init__(self,option):
+        self.option = option
+
+    def menuScreen(self):
+        print("Welcome To Harris Banking /n"
+              "Do you already have an Account (Y/N) "
+              )
